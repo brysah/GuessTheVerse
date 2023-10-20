4338755ea36af0aae7026eb8970308b9eb018a90
brysah <brysahvidal.com>
2023-10-20 20:13:17 -0300

    feat: added js functions for opening modal and closing

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var icone = document.getElementById('icone');
-  
-    icone.addEventListener('mouseover', function() {
-      icone.classList.remove('fa-regular');
-      icone.classList.add('fa-solid');
-    });
-  
-    icone.addEventListener('mouseout', function() {
-      icone.classList.remove('fa-solid');
-      icone.classList.add('fa-regular');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var icone = document.getElementById('icone');
+
+  icone.addEventListener('mouseover', function () {
+    icone.classList.remove('fa-regular');
+    icone.classList.add('fa-solid');
   });
-  
+
+  icone.addEventListener('mouseout', function () {
+    icone.classList.remove('fa-solid');
+    icone.classList.add('fa-regular');
+  });
+
+  const btnHow = document.querySelector('.how-to');
+  const close = document.querySelector('.icon-close');
+  const overlay = document.querySelector('.overlay');
+
+  function openModal() {
+    document.documentElement.classList.toggle('modal-opened');
+  }
+
+  btnHow.addEventListener('click', openModal);
+  overlay.addEventListener('click', openModal);
+  close.addEventListener('click',(e)=>{
+    e.preventDefault();
+    openModal();
+  });
+});

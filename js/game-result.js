37c25a0d401fdb54d47598bf09bc6e8b37e774b2
brysah <brysahvidal.com>
2023-10-26 17:01:14 -0300

    feat: added result game and comments about user's score

---
+++ js/game-result.js
@@ -0,0 +1,29 @@
+const headline = document.getElementById('headline');
+const message = document.getElementById('message');
+const score = parseInt(localStorage.getItem('points'));
+const playAgain = document.getElementById('play-again');
+
+
+if (score >= 0 && score <= 50){
+    headline.innerHTML = 'Nice try!';
+    message.innerHTML = "You're just getting started. Keep guessing and you'll get the hang of it! 🎵";
+}else if(score > 50 && score <= 75){
+    headline.innerHTML = "You're on fire!";
+    message.innerHTML = "Great job guessing those verses. Keep it up! 🔥🎶";
+}
+else if(score > 75 && score <= 100){
+    headline.innerHTML = "You're a 'Guesstheverse' pro!";
+    message.innerHTML = "You really know your stuff. Can you get a perfect score next time? 🎉🎵";
+}
+else{
+    headline.innerHTML = "Unbelievable! You aced it!";
+    message.innerHTML = "You're a 'Guesstheverse' superstar. Can anyone beat your score? 🌟🎤";
+}
+
+playAgain.addEventListener('click',()=>{
+    
+    localStorage.setItem('question',1);
+    localStorage.setItem('points',0);
+    window.location.href = 'game-setting.html';
+});
+
